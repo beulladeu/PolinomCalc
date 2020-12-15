29534002c82ee8c6686bea42498dccf640935e0a
beulladeu <vverkhonina@mail.ru>
2020-12-15 14:45:52 +0300

    new update

--- src/Main.java
+++ src/Main.java
@@ -4,13 +4,14 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 //Пути
-//C:\Users\beulladeu\IdeaProjects\Polinom\src\test\polinoms.txt
+//C:\Users\beulladeu\IdeaProjects\Polinom\src\subj\result.txt
 
 public class Main {
 
+    //фунцкция, разбирающая строку коэффициентов в массив
     public static double[] parse_string_to_array(String str){
         if (str.equals("")) str = "0";
-        String[] temp = str.split(" ");
+        String[] temp = str.split(" ");//делит строку на элементы с разделителем "пробел"
         double[] coefficients = new double[temp.length];
         for (int i = 0; i < coefficients.length; i++){
             coefficients[i] = Double.parseDouble(temp[i]);
@@ -18,6 +19,7 @@ public class Main {
         return coefficients;
     }
 
+    //фунцкция, формирующая строку коэффициентов из массива
     public static String parse_array_to_string(double[] coefficients){
         StringBuilder str = new StringBuilder();
         for (int i = 0; i < coefficients.length; i++) {
@@ -27,14 +29,16 @@ public class Main {
         return str.toString();
     }
 
+    //функция, считывания введенных коэффициентов и степени полинома с консоли
     public static double[] manual_writer_coefficients() throws IOException {
         StringBuilder inputLine = new StringBuilder();
+        //для получения пользовательского ввода
         Scanner sc = new Scanner(System.in);
 
         System.out.println("Введите степень многочлена");
         int pow = 0;
         while(true){
-            if (sc.hasNextInt()) {
+            if (sc.hasNextInt()) {//ожидается ввод, пока не будет введено целое число
                 pow = sc.nextInt();
                 if(pow <= 10 && pow >= 0) break;
                 else{
@@ -46,25 +50,28 @@ public class Main {
 
         System.out.println("Введите коэффициенты многочлена");
         while (pow != 0){
+            //ожидается ввод, пока не будет введено целое число, либо число типа double
             while (!(sc.hasNextDouble())){
                 sc.next();
                 System.out.println("Нужно вводить число");
             }
             double number = sc.nextDouble();
-            inputLine.append(number);
+            inputLine.append(number);//добавление числа в строку
             inputLine.append(" ");
             pow--;
         }
-
+        //возвращается массив, разобранный из строки
         return parse_string_to_array(inputLine.toString());
     }
 
+    //функция, обрабатывающая ввод пользователя
     public static int manual_writer_user_choice() throws IOException {
         int num = 0;
+        //для получения пользовательского ввода
         BufferedReader stdIn = new BufferedReader(new InputStreamReader(System.in));
         String inputLine = stdIn.readLine();
         try {
-            num = Integer.parseInt(inputLine.trim()); //trim удаляет пробелы в начале в конце строки
+            num = Integer.parseInt(inputLine.trim()); //trim удаляет пробелы в начале и в конце строки
         }
         catch (NumberFormatException ex) {
             System.out.println("Необходимо вводить цифры 1 или 2");
@@ -75,8 +82,10 @@ public class Main {
         return num;
     }
 
+    //функция, обрабатывающая выбор знака операции
     public static int manual_writer_math_operation() throws IOException {
         int num = 0;
+        //для получения пользовательского ввода
         BufferedReader stdIn = new BufferedReader(new InputStreamReader(System.in));
         String inputLine = stdIn.readLine();
         try {
@@ -91,6 +100,7 @@ public class Main {
         return num;
     }
 
+    //функция записи массива коэффициентов в файл
     public static void file_writer(String outputFileName, ArrayList<double[]> coefficients) throws IOException {
         try (BufferedWriter writer = new BufferedWriter(new FileWriter(outputFileName))) {
             for (int i = 0; i < coefficients.size(); i++) {
@@ -104,20 +114,22 @@ public class Main {
         }
     }
 
+    //функция чтения коэффициентов из файла в массив
     public static ArrayList<double[]> file_reader(String inputFileName){
         ArrayList<double[]> coefficients = new ArrayList<>();
         String line = "";
         try (BufferedReader reader = new BufferedReader(new FileReader(inputFileName))) {
             line = reader.readLine();
-            if (line == null) {
+            if (line == null) {//проверка файла на пустоту
                 System.out.println("Файл пуст.");
                 return null;
-                //exit(1); //программа закроется
             }
 
             double[] arr = parse_string_to_array(line);
             coefficients.add(arr);
             line = reader.readLine();
+
+            //разбор строки и запись коэффициентов в массив
             arr = parse_string_to_array(line);
             coefficients.add(arr);
         }
@@ -130,88 +142,97 @@ public class Main {
 
     public static void main(String[] args) throws IOException {
 
-        double[] coefficients1 = new double[0];
-        double[] coefficients2 = new double[0];
-        double[][] result = new double[2][0];
+        double[] coefficients1 = new double[0];//коэффициенты первого полинома
+        double[] coefficients2 = new double[0];//коэффициенты второго полинома
+        double[][] result = new double[2][0];//коэффициенты результирующего полинома
+
+        boolean file_work = false;// включает режим работы с файлами
+        boolean edit_file = false;// включает режим работы редактирования файлов
 
 
-        boolean file_work = false;
-        boolean edit_file = false;
-
-
+        //обрабатывает режим ввода вручную или из файла
         int num = 0;
         while(num == 0){
             System.out.println("1 - Ввести вручную");
             System.out.println("2 - Вывести из файла");
             num = manual_writer_user_choice();
-            if(num == 1){
+            if(num == 1){//ручной ввод
                 System.out.println("Первый многочлен");
                 coefficients1 = manual_writer_coefficients();
                 System.out.println("Второй многочлен");
                 coefficients2 = manual_writer_coefficients();
             }
             else if(num == 2){
+                // включает режим работы с файлами
                 file_work = true;
             }
         }
 
+        //если включен режим работы с файлами
         if(file_work){
             int num1 = 0;
             while(num1 == 0){
-                System.out.println("1 - Открыть файл"); //просто открыть
-                System.out.println("2 - Открыть файл на редактирование");//открыть на редактирование
+                System.out.println("1 - Открыть файл");
+                System.out.println("2 - Открыть файл на редактирование");
                 num1 = manual_writer_user_choice();
-                if(num1 == 1){
+                if(num1 == 1){//просто открыть и считать оттуда данные
                     System.out.println("Введите путь к файлу");
-
+                    //для пользовательского ввода с консоли
                     BufferedReader stdIn = new BufferedReader(new InputStreamReader(System.in));
                     String inputLine = stdIn.readLine();
                     File file = new File(inputLine);
-                    if(!file.exists()){
+                    if(!file.exists()){//проверка существует ли файл
                         System.out.println("Файл не существует. Укажите путь заново.");
                         num1 = 0;
                         continue;
                     }
 
                     ArrayList<double[]> arr = file_reader(inputLine);
+                    //проверка - если файл пуст, то пользователь возвращается в режим выбора
+                    //открыть файл или открыть файл на редактирование
                     if(arr == null || arr.isEmpty()) {
                         num1 = 0;
                         continue;
                     }
-                    coefficients1 = arr.get(0);
-                    coefficients2 = arr.get(1);
-
+                    coefficients1 = arr.get(0);//запись первого многочлема
+                    coefficients2 = arr.get(1);//запись второго многочлена
                     break;
                 }
-                else if(num1 == 2){
+                else if(num1 == 2){//открыть на редактирование и считать новые данные
+                    // включает режим работы редактирования файла
                     edit_file = true;
                 }
             }
         }
 
+        //если включен режим работы редактирования файлов
         if(edit_file){
             int num1 = 0;
             while(num1 == 0) {
                 num1 = 1;
-
                 System.out.println("Введите путь к файлу");
+                //для пользовательского ввода с консоли
                 BufferedReader stdIn = new BufferedReader(new InputStreamReader(System.in));
                 String inputLine = stdIn.readLine();
                 File file = new File(inputLine);
+                //проверка существования файла
+                //то пользователь возвращается в режим ввода пути к файлу
                 if (!file.exists()) {
                     System.out.println("Файл не существует. Укажите путь заново.");
                     num1 = 0;
                     continue;
                 }
+                //вывод на экран содержимого файла
                 System.out.println("Содержимое файла:");
                 ArrayList<double[]> data = file_reader(inputLine);
-                if(data != null) {
+                if(data != null) {//проверка не был ли файл пустым
                     System.out.println(Arrays.toString(data.get(0)));
                     System.out.println(Arrays.toString(data.get(1)));
+                }else {
+                    System.out.println("Файл пуст");
                 }
 
                 System.out.println("Введите новые значения коэффициентов, которые затем запишутся в файл");
-
                 System.out.println("Первый многочлен");
                 coefficients1 = manual_writer_coefficients();
                 System.out.println("Второй многочлен");
@@ -220,11 +241,12 @@ public class Main {
                 ArrayList<double[]> arr = new ArrayList<>();
                 arr.add(coefficients1);
                 arr.add(coefficients2);
-                file_writer(inputLine, arr);
+                file_writer(inputLine, arr);//запись новых значений полиномов в файл
             }
         }
 
 
+        //обработка ввода математической операции
         num = 0;
         while(num == 0){
             System.out.println("Выберите математическую операцию:");
@@ -239,74 +261,53 @@ public class Main {
             System.out.println(a);
             System.out.println(b);
 
-            if (num == 1){
+            if (num == 1){//сложение
                 Polynomial sum = a.sum(b);
-                result[0] = sum.getPolynomial();
                 System.out.print("Результат ");
                 System.out.println(sum);
+                result[0] = sum.reverseArray(sum.getPolynomial());
             }
-            else if(num == 2){
+            else if(num == 2){//вычитание
                 Polynomial diff = a.diff(b);
-                result[0] = diff.getPolynomial();
                 System.out.print("Результат ");
                 System.out.println(diff);
+                result[0] = diff.reverseArray(diff.getPolynomial());
             }
-            else if(num == 3){
+            else if(num == 3){//умножение
                 Polynomial mult = a.mult(b);
-                result[0] = mult.getPolynomial();
                 System.out.print("Результат ");
                 System.out.println(mult);
+                result[0] = mult.reverseArray(mult.getPolynomial());
             }
-            else if(num == 4){
+            else if(num == 4){//деление
                 Polynomial[] divide = a.divide(b);
-                result[0] = divide[0].getPolynomial();
-                result[1] = divide[1].getPolynomial();
                 System.out.print("Результат ");
                 System.out.println(Arrays.toString(divide));
+                result[0] = divide[0].reverseArray(divide[0].getPolynomial());
+                result[1] = divide[1].reverseArray(divide[1].getPolynomial());
             }
         }
 
 
+        //обработка выбора пользователя сохранять результат в файл или нет
         num = 0;
         while(num == 0){
             System.out.println("1 - Сохранить результат в файл");
             System.out.println("2 - Не сохранять");
-            num = manual_writer_user_choice();
+            num = manual_writer_user_choice();//обработка ввода пользователя
             if(num == 1){
                 System.out.println("Введите путь к файлу");
+                //для пользовательского ввода пути к файлу с консоли
                 BufferedReader stdIn = new BufferedReader(new InputStreamReader(System.in));
                 String inputLine = stdIn.readLine();
-
                 ArrayList<double[]> arr = new ArrayList<>();
                 arr.add(result[0]);
                 arr.add(result[1]);
-                file_writer(inputLine, arr);
+                file_writer(inputLine, arr);//запись коэффициентов в файл
             }
             else if(num == 2){
                 break;
             }
         }
-/*
-        //тесты
-        double[] coefficients1 = new double[] { 5, 2, 1, 6 };
-        Polynomial a = new Polynomial(coefficients1.length, coefficients1);
-
-        double[] coefficients2 = new double[] { 1, 0, 3 };
-        Polynomial b = new Polynomial(coefficients2.length, coefficients2);
-
-        Polynomial test_sum = a.sum(b);
-
-        Polynomial test_mult = a.mult(b);
-
-        Polynomial test_diff = a.diff(b);
-
-        Polynomial[] test_divide = a.divide(b);
-
-
-        System.out.println(test_sum);
-        System.out.println(test_mult);
-        System.out.println(test_diff);
-        System.out.println(Arrays.toString(test_divide));
-*/
     }
 }
--- src/Polynomial.java
+++ src/Polynomial.java
@@ -3,46 +3,55 @@ import java.util.Arrays;
 import static java.lang.System.exit;
 
 public class Polynomial {
-    private int pow;
-    private double[] polynomial;
+    private int pow; //степень полинома
+    private double[] polynomial; //коэффициенты полинома
 
-
+    //конструктор объекта Polinomial
+    //с инициализацией степени и коэффициентов полинома
     public Polynomial(int size, double[] array){
         this.pow = size;
         this.polynomial = new double[this.pow];
-        this.polynomial = this.reverseArray(array);
 
+        //для удобства реализации операций над полиномами
+        //массив коэффициенов записывается в обратном порядке
+        this.polynomial = this.reverseArray(array);
     }
 
+    //конструктор объекта Polinomial
+    //c инициализацией полинома заданной степени, заполненного нулями
     private Polynomial(int pow) {
         this.pow = pow;
         this.polynomial = new double[this.pow];
     }
 
-    public Polynomial sum(Polynomial a){
+    public Polynomial sum(Polynomial a){//метод сложения двух полиномов
         double[] res = new double[Math.max(this.pow, a.getPow())];
         int min_size = Math.min(this.pow, a.getPow());
-
         for (int i = 0; i < min_size; i++) {
+            //коэффициенты полиномов складываются
             res[i] = this.polynomial[i] + a.getPolynomial()[i];
         }
+        //если полиномы разных степеней, то старшие коэффициенты большего полинома
+        // копируются в начало результирующего полинома
         if (this.pow > a.pow) System.arraycopy(this.polynomial, min_size, res, min_size, res.length-min_size);
         if (a.pow > this.pow) System.arraycopy(a.polynomial, min_size, res, min_size, res.length-min_size);
         res = this.reverseArray(res);
         return new Polynomial(res.length, res);
     }
 
-    public Polynomial diff(Polynomial a) {
+    public Polynomial diff(Polynomial a) {//метод разности полиномов
         Polynomial p = new Polynomial(a.getPolynomial().length, this.reverseArray(a.getPolynomial()));
+        //коэффициенты вычитаемого полинома меняют знак на противоположный
+        //затем уменьшаемый полином складывается с вычитаемым полиномом
         for (int i = 0; i < p.getPolynomial().length; i++) {
             p.getPolynomial()[i] = p.getPolynomial()[i] * (-1);
         }
         return this.sum(p);
     }
 
-    public Polynomial mult(Polynomial a){
+    public Polynomial mult(Polynomial a){//метод умножения полиномов
         double[] res = new double[this.pow + a.getPow()];
-
+        //сумма умножения каждого элемента одного полинома с каждым элементом второго полинома
         for (int i = this.pow - 1; i >= 0; i--) {
             for (int j = a.getPow() - 1; j >= 0; j--) {
                 res[i + j] += this.polynomial[i] * a.getPolynomial()[j];
@@ -52,7 +61,7 @@ public class Polynomial {
         return new Polynomial(res.length, res);
     }
 
-    private boolean is_zero(Polynomial a){
+    private boolean is_zero(Polynomial a){//метод проверки полинома на заполненность нулями
         for (int i = 0; i < a.polynomial.length; i++){
             if(a.polynomial[i] != 0) return false;
         }
@@ -60,14 +69,15 @@ public class Polynomial {
     }
 
 
-    public Polynomial[] divide(Polynomial a) {
-        if(this.is_zero(a)){
+    public Polynomial[] divide(Polynomial a) {//метод деления полиномов
+        if(this.is_zero(a)){//проверка деления на ноль
             System.out.println("Zero division");
             exit(1);
         }
         Polynomial temp = this;
         int m = a.getPow() - 1;
         Polynomial res = new Polynomial(temp.getPow() - a.getPow() + 1);
+        //алгоритм деления полиномов столбиком
         for (int i = temp.getPow() - 1; i >= m; i--) {
             if(a.getPolynomial()[m] == 0) {
                 continue;
@@ -83,7 +93,7 @@ public class Polynomial {
         return new Polynomial[]{res, temp};
     }
 
-    private double[] reverseArray(double[] arr) {
+    public double[] reverseArray(double[] arr) {//метод записи массива в обратном порядке
         int j = 0;
         double[] res = new double[arr.length];
         for (int i = arr.length - 1; i >= 0; i--, j++) {
@@ -92,24 +102,27 @@ public class Polynomial {
         return res;
     }
 
+    //метод, возвращающий коэффициенты полинома
     public double[] getPolynomial() {
         return polynomial;
     }
 
-    public int getPow() {
+    public int getPow() {//метод, возвращающий степень полинома
         return pow;
     }
 
-
+    //метод, возвращающий строку красивой записи полинома
     @Override
     public String toString() {
         if(this.is_zero(this)){
             return "0";
         }
         StringBuilder s = new StringBuilder("");
-        boolean flag = false;
         for (int i = this.pow - 1; i >= 0; i--) {
             if(this.polynomial[i] == 0) continue;
+            if(this.polynomial[i] < 0 && i != this.pow - 1){
+                s = new StringBuilder(s.substring(0, s.length() - 1));
+            }
             s.append(this.polynomial[i]);
             if (i == 1) {
                 s.append("x");
@@ -118,13 +131,16 @@ public class Polynomial {
                 s.append("x^");
                 s.append(i);
             }
-            if(this.polynomial[i] > 0)s.append(" ");
+
+            s.append(" ");
         }
         String str = s.toString().trim();
-        str = str.replaceAll(" ", "+");
-        return str;
+        str = str.replaceAll(" ", " + ");
+        str = str.replaceAll("-", " -");
+        return str.trim();
     }
 
+    //метод, проверяющий полиномы на равенство
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
